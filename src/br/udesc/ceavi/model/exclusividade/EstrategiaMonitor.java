70c780ace1410d8341acc87e8bab5a4cbae995d1
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2018-09-23 20:02:00 -0300

    classe base de estratégia

--- src/br/udesc/ceavi/model/exclusividade/EstrategiaMonitor.java
+++ src/br/udesc/ceavi/model/exclusividade/EstrategiaMonitor.java
@@ -2,7 +2,6 @@ package br.udesc.ceavi.model.exclusividade;
 
 import br.udesc.ceavi.model.bo.ModelMovimentoVeiculo;
 import br.udesc.ceavi.model.entity.Coordenada;
-import br.udesc.ceavi.model.entity.MalhaViaria;
 import br.udesc.ceavi.model.entity.Veiculo;
 import br.udesc.ceavi.model.entity.Via;
 import com.sun.istack.internal.NotNull;
@@ -14,61 +13,25 @@ import java.util.Random;
  * Estratégia para adquirir "lock" utilizando Monitores
  * @author lucas.adriano
  */
-public class EstrategiaMonitor implements EstrategiaExclusividade {
-
-    private MalhaViaria malhaViaria;
+public class EstrategiaMonitor extends EstrategiaPadrao {
 
     @Override
-    public void setMalhaViaria(MalhaViaria malhaViaria) {
-        this.malhaViaria = malhaViaria;
-    }
+    protected synchronized void setViaVeiculo(@NotNull Veiculo veiculo, @NotNull ModelMovimentoVeiculo movimentoVeiculo) {
+        List<Via> vias   = movimentoVeiculo.getProximasVias();
+        Random random    = new Random();
+        int proximaVia   = random.nextInt(vias.size());
+        Via viaNova      = vias.get(proximaVia);
+        Coordenada cruzamento  = viaNova.getPontoInicial();
+        Coordenada proximaCruz = movimentoVeiculo.getNext(cruzamento, viaNova);
 
-    @Override
-    public void movimentaVeiculo(Veiculo veiculo) {
-        Via via = veiculo.getVia();
-        ModelMovimentoVeiculo movimentoVeiculo = new ModelMovimentoVeiculo(veiculo, malhaViaria);
-
-        Coordenada coordenada = movimentoVeiculo.getNext(veiculo.getCoordenada(), via);
-
-        //Caso o veículo tenha chegado ao final da via e esta via não seja uma de saída
-        if (coordenada == null) {
-            if (!malhaViaria.getViasSaida().contains(via)) {
-                List<Via> vias   = movimentoVeiculo.getProximasVias();
-                Random random    = new Random();
-                int proximaVia   = random.nextInt(vias.size());
-                Via viaNova      = vias.get(proximaVia);
-                Coordenada coor  = viaNova.getPontoInicial();
-                Coordenada coor2 = movimentoVeiculo.getNext(coor, viaNova);
-
-                //Tenta adquirir o lock do cruzamento somente se o cruzamento e a próxima via está liberada
-                if (coor.isLiberada() && coor2.isLiberada()) {
-                    setViaVeiculo(viaNova, veiculo);
-                }
-            }
-            else {
-                veiculo.setEmMovimento(false);
-            }
-        }
-        else if (coordenada.isLiberada()) {
-            setCoordenadaVeiculo(coordenada, veiculo);
+        //Tenta adquirir o lock do cruzamento somente se o cruzamento e a próxima via está liberada
+        if (cruzamento.isLiberada() && proximaCruz.isLiberada()) {
+            veiculo.setVia(viaNova);
         }
     }
 
-    /**
-     * Atualiza a via do veículo
-     * @param via
-     * @param veiculo
-     */
-    private synchronized void setViaVeiculo(Via via, @NotNull Veiculo veiculo) {
-        veiculo.setVia(via);
-    }
-
-    /**
-     * Atualiza a coordenada do veículo
-     * @param coordenada
-     * @param veiculo
-     */
-    private synchronized void setCoordenadaVeiculo(Coordenada coordenada, @NotNull Veiculo veiculo) {
+    @Override
+    protected synchronized void setCoordenadaVeiculo(Coordenada coordenada, @NotNull Veiculo veiculo) {
         veiculo.setCoordenada(coordenada);
     }
 }
---
+++ src/br/udesc/ceavi/model/exclusividade/EstrategiaPadrao.java
@@ -0,0 +1,56 @@
+package br.udesc.ceavi.model.exclusividade;
+
+import br.udesc.ceavi.model.bo.ModelMovimentoVeiculo;
+import br.udesc.ceavi.model.entity.Coordenada;
+import br.udesc.ceavi.model.entity.MalhaViaria;
+import br.udesc.ceavi.model.entity.Veiculo;
+import br.udesc.ceavi.model.entity.Via;
+
+/**
+ * Classe padrão de estratégias de exclusividade
+ * @author lucas.adriano
+ */
+abstract public class EstrategiaPadrao implements EstrategiaExclusividade {
+
+    private MalhaViaria malhaViaria;
+
+    @Override
+    public void setMalhaViaria(MalhaViaria malhaViaria) {
+        this.malhaViaria = malhaViaria;
+    }
+
+    @Override
+    public void movimentaVeiculo(Veiculo veiculo) {
+        Via via = veiculo.getVia();
+        ModelMovimentoVeiculo movimentoVeiculo = new ModelMovimentoVeiculo(veiculo, malhaViaria);
+
+        Coordenada coordenada = movimentoVeiculo.getNext(veiculo.getCoordenada(), via);
+
+        //Caso o veículo tenha chegado ao final da via e esta via não seja uma de saída
+        if (coordenada == null) {
+            if (!malhaViaria.getViasSaida().contains(via)) {
+                setViaVeiculo(veiculo, movimentoVeiculo);
+            }
+            else {
+                veiculo.setEmMovimento(false);
+            }
+        }
+        else if (coordenada.isLiberada()) {
+            setCoordenadaVeiculo(coordenada, veiculo);
+        }
+    }
+
+    /**
+     * Atualiza a via do veículo
+     * @param veiculo
+     * @param movimentoVeiculo
+     */
+    protected abstract void setViaVeiculo(Veiculo veiculo, ModelMovimentoVeiculo movimentoVeiculo);
+
+    /**
+     * Atualiza a coordenada do veículo
+     * @param coordenada
+     * @param veiculo
+     */
+    protected abstract void setCoordenadaVeiculo(Coordenada coordenada, Veiculo veiculo);
+}
