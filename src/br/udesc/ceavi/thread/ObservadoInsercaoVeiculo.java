86dc695c960d9c8aa0c697286ce4334f9bd5c4f9
Lucas Felicio Adriano <lucas.f.adriano@hotmail.com>
2018-09-23 20:59:03 -0300

    correção para retirar os veículos da via

---
+++ src/br/udesc/ceavi/thread/ObservadoInsercaoVeiculo.java
@@ -0,0 +1,17 @@
+package br.udesc.ceavi.thread;
+
+import br.udesc.ceavi.model.entity.Veiculo;
+
+/**
+ * Observador de inserção de veículos
+ * @author lucas.adriano
+ */
+public interface ObservadoInsercaoVeiculo {
+
+    /**
+     * Indica que o veículo foi inserido na malha
+     * @param veiculo
+     */
+    public void veiculoJnserido(Veiculo veiculo);
+
+}
